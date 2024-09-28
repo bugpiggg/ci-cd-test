944f057a5b02f6aedfa07d59b0a67e9bfd8226ee
jongin <jonginwon1203@gmail.com>
2024-09-28 15:38:57 +0900

    deploy: server first deploy

---
+++ src/main/java/com/jongin/cicid/controller/HealthCheckController.java
@@ -0,0 +1,38 @@
+package com.jongin.cicid.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+public class HealthCheckController {
+
+    @Value("${serverName}")
+    private String serverName;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${server.port}")
+    private String port;
+    @Value("${server.env}")
+    private String env;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("serverName", serverName);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("port", port);
+        responseData.put("env", env);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
