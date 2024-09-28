342d9b66c05d47359bbe986c0614d101f3a95829
jongin <jonginwon1203@gmail.com>
2024-09-28 13:34:55 +0900

    first commit

---
+++ src/main/java/com/jongin/cicid/CicidApplication.java
@@ -0,0 +1,13 @@
+package com.jongin.cicid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongin/cicid/CicidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongin.cicid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
